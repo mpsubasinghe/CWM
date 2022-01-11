172178d0bd75f51272a017629c18e8035e42d646
mpsubasinghe <91259524+mpsubasinghe@users.noreply.github.com>
2022-01-11 09:47:31 +0530

    Frist Commit

---
+++ CWM_Change_EffectiveDate/Form1.Designer.vb
@@ -0,0 +1,141 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.ComboBoxDiv = New System.Windows.Forms.ComboBox()
+        Me.ComboBoxCompany = New System.Windows.Forms.ComboBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.DateTimePicker1 = New System.Windows.Forms.DateTimePicker()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(18, 130)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(75, 13)
+        Me.Label4.TabIndex = 67
+        Me.Label4.Text = "Effective Date"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(18, 103)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(83, 13)
+        Me.Label3.TabIndex = 65
+        Me.Label3.Text = "PDA  ProductID"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(18, 76)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(72, 13)
+        Me.Label2.TabIndex = 64
+        Me.Label2.Text = "Division Code"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(18, 48)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(79, 13)
+        Me.Label1.TabIndex = 63
+        Me.Label1.Text = "Company Code"
+        '
+        'ComboBoxDiv
+        '
+        Me.ComboBoxDiv.FormattingEnabled = True
+        Me.ComboBoxDiv.Location = New System.Drawing.Point(107, 73)
+        Me.ComboBoxDiv.Name = "ComboBoxDiv"
+        Me.ComboBoxDiv.Size = New System.Drawing.Size(205, 21)
+        Me.ComboBoxDiv.TabIndex = 62
+        '
+        'ComboBoxCompany
+        '
+        Me.ComboBoxCompany.FormattingEnabled = True
+        Me.ComboBoxCompany.Location = New System.Drawing.Point(107, 45)
+        Me.ComboBoxCompany.Name = "ComboBoxCompany"
+        Me.ComboBoxCompany.Size = New System.Drawing.Size(205, 21)
+        Me.ComboBoxCompany.TabIndex = 61
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(249, 221)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 60
+        Me.Button1.Text = "Update"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'DateTimePicker1
+        '
+        Me.DateTimePicker1.Location = New System.Drawing.Point(107, 124)
+        Me.DateTimePicker1.Name = "DateTimePicker1"
+        Me.DateTimePicker1.Size = New System.Drawing.Size(205, 20)
+        Me.DateTimePicker1.TabIndex = 59
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(107, 98)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(205, 20)
+        Me.TextBox1.TabIndex = 68
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(336, 256)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.ComboBoxDiv)
+        Me.Controls.Add(Me.ComboBoxCompany)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.DateTimePicker1)
+        Me.Name = "Form1"
+        Me.Text = "Product Update"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents ComboBoxDiv As System.Windows.Forms.ComboBox
+    Friend WithEvents ComboBoxCompany As System.Windows.Forms.ComboBox
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents DateTimePicker1 As System.Windows.Forms.DateTimePicker
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+
+End Class
---
+++ CWM_Change_EffectiveDate/Form1.vb
@@ -0,0 +1,57 @@
+﻿
+Imports System.Data.SqlClient
+Public Class Form1
+    Dim Xds As New DataSet()
+    Dim ds As New DataSet()
+    Dim ds1 As New DataSet()
+    Dim ds2 As New DataSet()
+    Dim ds3 As New DataSet()
+    Dim ds4 As New DataSet()
+    Dim ds5 As New DataSet()
+    Private Sub TextBox1_TextChanged(sender As System.Object, e As System.EventArgs) Handles TextBox1.TextChanged
+
+    End Sub
+
+    Private Sub Form1_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
+        con.Open()
+        LoadCombo(GetDataSQL("p1", "p2", "p3", "p4", "p5", "@rd", "pn2", "pn3", "pn4", "pn5", "SELECT CompanyCode FROM DisGroupType GROUP BY CompanyCode  ORDER BY CompanyCode"), ComboBoxCompany, "CompanyCode")
+        LoadCombo(GetDataSQL("p1", "p2", "p3", "p4", "p5", "@rd", "pn2", "pn3", "pn4", "pn5", "SELECT DivCode FROM DisGroupType GROUP BY DivCode  ORDER BY DivCode"), ComboBoxDiv, "DivCode")
+        ComboBoxCompany.SelectedIndex = 0
+        ComboBoxDiv.SelectedIndex = 0
+    End Sub
+
+    Private Sub Button1_Click(sender As System.Object, e As System.EventArgs) Handles Button1.Click
+
+
+        'Dim com19 As New SqlCommand("UPDATE Product SET EffectiveDate = @a1 where  PDAProductCode = '" & TextBox1.Text & "' AND CompanyCode='" & ComboBoxCompany.Text & "' AND DivCode= '" & ComboBoxDiv.Text & "' ", con)
+        'com19.Parameters.AddWithValue("@a1", DateTimePicker1.Value.Date.ToString("yyyy-MM-dd"))
+        'com19.ExecuteNonQuery()
+
+
+        '' '' ''        ds = GetDataSQL("", "p2", "p3", "p4", "p5", "@dt", "pn2", "pn3", "pn4", "pn5",
+        '' '' ''"select * from Test_1 ")
+        '' '' ''        If ds.Tables(0).Rows.Count > 0 Then
+        '' '' ''            For a As Integer = 0 To ds.Tables(0).Rows.Count - 1
+
+
+        '' '' ''                '                ds1 = GetDataSQL("", "p2", "p3", "p4", "p5", "@dt", "pn2", "pn3", "pn4", "pn5",
+        '' '' ''                '"select * from MaterialMaster where  MaterialNumber = '" & ds.Tables(0).Rows(a)("PID").ToString() & "' AND SalesOrg='2000'  ")
+        '' '' ''                '                If ds1.Tables(0).Rows.Count > 0 Then
+        '' '' ''                '                    MsgBox(ds.Tables(0).Rows(a)("PID").ToString() & "  " & ds1.Tables(0).Rows.Count)
+        '' '' ''                '                End If
+
+        '' '' ''                Dim com19 As New SqlCommand("UPDATE MaterialMaster SET StorageLocation = @a1,SUpdated='False' where plant='2003' AND  MaterialNumber = '" & ds.Tables(0).Rows(a)("PID").ToString() & "' AND SalesOrg='2000' ", con)
+        '' '' ''                com19.Parameters.AddWithValue("@a1", "1630")
+        '' '' ''                com19.ExecuteNonQuery()
+
+        '' '' ''            Next
+        '' '' ''        End If
+
+
+       
+
+        MsgBox("Done")
+
+
+    End Sub
+End Class
